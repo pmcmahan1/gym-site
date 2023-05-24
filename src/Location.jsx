9de3da0aba3023ad09498d27aa64c3555c466c0e
unknown <mcmahanconner@gmail.com>
2023-05-24 19:18:58 -0400

    Maps added

--- src/Location.jsx
+++ src/Location.jsx
@@ -1,15 +1,24 @@
-import React, { useMemo } from 'react'
-import { GoogleMap, useLoadScript, Marker} from "@react-google-maps/api"
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import './Location.scss'
 import googleIcon from './assets/google-icon.png'
 import googleMaps from './assets/google-maps.png'
 import starFull from './assets/star-full.png'
 import starHalf from './assets/star-half.png'
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import PhoneIcon from '@mui/icons-material/Phone';
 
 const Location = () => {
 
-
+    const containerStyle = {
+        width: '400px',
+        height: '400px'
+      };
+      
+      const center = {
+        lat: 39.1031,
+        lng: -84.5120
+      };
 
   return (
     <div className='location'>
@@ -17,6 +26,8 @@ const Location = () => {
         <div className="location-container">
             <div className="location-container-left">
             <div className="location-name">Gym Name Cincinnati
+            </div>
+            <div className="location-review">
             <img src={googleIcon} alt="" className='icon'/>
             <img src={starFull} alt="" className='icon'/>
             <img src={starFull} alt="" className='icon'/>
@@ -28,6 +39,10 @@ const Location = () => {
                 <PhoneIcon className='phone-icon' />
                 (888) 888-8888
             </div>
+            <div className="location-number">
+                <AccessTimeIcon className='phone-icon'/>
+                Hours: Open 24 Hours
+            </div>
             <div className="location-address">
             <img src={googleMaps} alt="" className='icon'/>
             243 Euclid St.
@@ -35,7 +50,16 @@ const Location = () => {
             </div>
         </div>
         <div className="location-container-right">
-            Map
+        <LoadScript
+        googleMapsApiKey="AIzaSyB1yiB1XKpZ9hneEWyvoY5wTldQ3Ba-lMU"
+      >
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={12}
+        >
+        </GoogleMap>
+      </LoadScript>
         </div>
         </div>
     </div>
